6af66e057f964a6c266798dc195c206a0a2507f0
Eugene Popov <popov895@ukr.net>
2024-03-07 11:06:02 +0200

    Set initial key focus to the appropriate button

--- extension.js
+++ extension.js
@@ -12,14 +12,23 @@ export default class extends Extension {
             return function () {
                 this.clearButtons();
 
-                this.addButton({
+                const addButton = (buttonInfo) => {
+                    const button = this.addButton(buttonInfo);
+                    if (buttonInfo.setKeyFocus) {
+                        this.setInitialKeyFocus(button);
+                    }
+                    return button;
+                };
+
+                addButton({
                     label: _(`Cancel`),
                     key: Clutter.KEY_Escape,
                     action: this.cancel.bind(this),
                 });
 
-                this.addButton({
+                addButton({
                     label: _(`Log Out`),
+                    setKeyFocus: this._type === 0,
                     action: () => {
                         const signalId = this.connect(`closed`, () => {
                             this.disconnect(signalId);
@@ -30,8 +39,9 @@ export default class extends Extension {
                 });
 
                 const rebootAndInstall = this._pkOfflineProxy && (this._updateInfo.UpdateTriggered || this._updateInfo.UpgradeTriggered);
-                this._rebootButton = this.addButton({
+                this._rebootButton = addButton({
                     label: rebootAndInstall ? _(`Restart &amp; Install`) : _(`Restart`),
+                    setKeyFocus: this._type >= 2 && this._type <= 4,
                     action: () => {
                         const signalId = this.connect(`closed`, () => {
                             this.disconnect(signalId);
@@ -42,7 +52,7 @@ export default class extends Extension {
                 });
 
                 if (this._canRebootToBootLoaderMenu) {
-                    this._rebootButtonAlt = this.addButton({
+                    this._rebootButtonAlt = addButton({
                         label: _(`Boot Options`),
                         action: () => {
                             const signalId = this.connect(`closed`, () => {
@@ -56,8 +66,9 @@ export default class extends Extension {
                     this._capturedEventId = this.connect(`captured-event`, this._onCapturedEvent.bind(this));
                 }
 
-                this.addButton({
+                addButton({
                     label: _(`Power Off`),
+                    setKeyFocus: this._type === 1,
                     action: () => {
                         const signalId = this.connect(`closed`, () => {
                             this.disconnect(signalId);
